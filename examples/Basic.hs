6f77c309705f1adec415049fa342b500b142261e
Johan Tibell <johan.tibell@gmail.com>
2014-04-24 23:10:28 +0200

    Improve example

--- examples/Basic.hs
+++ examples/Basic.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main
     ( main
     ) where
@@ -6,6 +7,7 @@ import Control.Concurrent (threadDelay)
 import Control.Exception (evaluate)
 import Data.List (foldl')
 import System.Metrics
+import qualified System.Metrics.Counter as Counter
 import System.Remote.Monitoring.Statsd
 
 -- 'sum' is using a non-strict lazy fold and will blow the stack.
@@ -18,9 +20,12 @@ mean xs = sum' xs / fromIntegral (length xs)
 main :: IO ()
 main = do
     store <- newStore
+    registerGcMetrics store
+    iters <- createCounter "iterations" store
     forkStatsd defaultStatsdOptions store
     let loop n = do
             evaluate $ mean [1..n]
+            Counter.inc iters
             threadDelay 2000
             loop n
     loop 1000000
